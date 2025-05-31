2e127a4b6b5869427d64c82f9b6b247bafdd8413
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-05-31 19:04:00 +0330

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,29 +3,15 @@
 namespace MohammadAlavi\ConfigSync;
 
 use Composer\Composer;
-use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\Capability\CommandProvider;
 use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
 
-final class Plugin implements PluginInterface, Capable, EventSubscriberInterface
+final class Plugin implements PluginInterface, Capable
 {
-    private Composer $composer;
-
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            'post-autoload-dump' => 'registerPlugins',
-        ];
-    }
-
     public function activate(Composer $composer, IOInterface $io): void
     {
-        $io->write('Activating ConfigSyncPlugin...');
-        $this->composer = $composer;
     }
 
     public function getCapabilities(): array
@@ -41,13 +27,5 @@ final class Plugin implements PluginInterface, Capable, EventSubscriberInterface
 
     public function uninstall(Composer $composer, IOInterface $io): void
     {
-    }
-
-    public function registerPlugins(): void
-    {
-        \Safe\fwrite(STDERR, "Registering ConfigSyncPlugin...\n");
-        $cmd = new Command();
-        $cmd->setComposer($this->composer);
-        $cmd->run(new ArrayInput([]), new ConsoleOutput(ConsoleOutput::VERBOSITY_NORMAL, true));
     }
 }
