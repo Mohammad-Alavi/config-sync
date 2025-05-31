1b8150fe55d197e77b497b7011082f6a4d26d329
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-05-31 19:42:05 +0330

    Update CommandProvider.php

--- src/CommandProvider.php
+++ src/CommandProvider.php
@@ -3,13 +3,16 @@
 namespace MohammadAlavi\ConfigSync;
 
 use Composer\Plugin\Capability\CommandProvider as CommandProviderCapability;
+use MohammadAlavi\ConfigSync\Commands\Init;
+use MohammadAlavi\ConfigSync\Commands\Sync;
 
 final class CommandProvider implements CommandProviderCapability
 {
     public function getCommands(): array
     {
         return [
-            new Command(),
+            new Sync(),
+            new Init(),
         ];
     }
 }
